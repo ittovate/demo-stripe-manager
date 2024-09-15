01494ecd4cd96edbb59246255c3d723dac95cb44
mustafa-abuelmagd <mustafa-abuelmagd@gmail.com>
2024-09-15 06:36:55 +0300

    feat: added price controller and price service

---
+++ src/main/java/com/example/stripemanager/controllers/PriceController.java
@@ -0,0 +1,69 @@
+package com.example.stripemanager.controllers;
+
+
+import com.example.stripemanager.services.PriceService;
+import com.example.stripemanager.utils.RestResponse;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Price;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("prices")
+public class PriceController {
+
+    @Autowired
+    PriceService priceService;
+
+    public PriceController(@Value("${stripe.apiKey}")
+                             String apiKey) {
+        Stripe.apiKey = apiKey;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<RestResponse<Price>> addPrice(@RequestBody Price price) throws StripeException {
+        System.out.println(price.toJson());
+
+        RestResponse<Price> response = new RestResponse<>(priceService.addPrice(price), "", HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestResponse<Price>> getPriceById(@PathVariable String id) throws StripeException {
+        RestResponse<Price> response = new RestResponse<>(priceService.getPriceById(id), "", HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/")
+    public ResponseEntity<RestResponse<List<Price>>> listPrices(
+            @RequestParam(required = false) Boolean active,
+            @RequestParam(required = false) String product,
+            @RequestParam(required = false) String endingBefore,
+            @RequestParam(required = false) Long limit,
+            @RequestParam(required = false) String startingAfter,
+            @RequestParam(required = false) String url
+    ) throws StripeException {
+
+        List<Price> prices = priceService.listPrices(
+                active,
+                product,
+                endingBefore,
+                limit,
+                startingAfter
+        );
+
+        RestResponse<List<Price>> response = new RestResponse<>(prices, "", HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+}
