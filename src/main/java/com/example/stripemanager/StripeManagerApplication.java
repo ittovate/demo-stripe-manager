3cb00b9a04f6e225e907fa0d24189f0c79b70f28
mustafa-abuelmagd <mustafa-abuelmagd@gmail.com>
2024-09-15 03:09:58 +0300

    feat: app set up

---
+++ src/main/java/com/example/stripemanager/StripeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stripemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stripemanager/StripeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stripemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
